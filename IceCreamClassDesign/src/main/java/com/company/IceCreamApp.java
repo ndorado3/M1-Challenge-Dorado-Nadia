1be676ddf93a6a1d4f9cecf14e1dba3a3008a678
nadiadorado@gmail.com <ndorado@uncc.edu>
2022-02-18 16:45:33 -0500

    added code to iceCreamApp class

--- IceCreamClassDesign/src/main/java/com/company/IceCreamApp.java
+++ IceCreamClassDesign/src/main/java/com/company/IceCreamApp.java
@@ -3,5 +3,16 @@ package com.company;
 public class IceCreamApp {
     public static void main(String[] args) {
 
+        com.company.factory.IceCream newIceCreamFlavor = new com.company.factory.IceCream(
+                "Passion Fruit", 4.96,10, 2, "Milk, Passion Fruit, Salt");
+
+        System.out.println(
+                "New Flavor: " + newIceCreamFlavor.getFlavor() + "\n" +
+                "Sale Price: " + "$" + newIceCreamFlavor.getSalePrice() + "\n" +
+                        "Production Cost: " + "$" + newIceCreamFlavor.getProductionCost() + "\n" +
+                        "Production Time: " + newIceCreamFlavor.getProductionTime() + " hours" +"\n" +
+                        "Ingredients: " + newIceCreamFlavor.getIngredients()
+        );
+
     }
 }
